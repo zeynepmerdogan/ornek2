d4896ddc68c3c47ba65d8ce2f6ff4d476fc9acc3
zeynepmerdogan <zeynepmelekerdogan@gmail.com>
2020-11-24 19:34:25 +0100

    version 1

---
+++ src/test1.java
@@ -0,0 +1,6 @@
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println(2);
+        System.out.println(".gitignore dosyasina hangi dosyalarin ve ya klasörlerin gitmeyecegine");
+    }
+}
